87a2efdc2d17a42717ca9bbc51def7cb5069d9e5
dinkarl <dineshalwaysnew@gmail.com>
2021-07-10 19:47:58 +0530

    Update AuthenticationFilter.java

--- src/main/java/com/organizationmanagement/interceptor/AuthenticationFilter.java
+++ src/main/java/com/organizationmanagement/interceptor/AuthenticationFilter.java
@@ -48,6 +48,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter
                 .setExpiration(new Date(System.currentTimeMillis() + 864_000_000))
                 .signWith(SignatureAlgorithm.HS512, "SecretKeyToGenJWTs".getBytes())
                 .compact();
-        response.addHeader("Authorization","Bearer " + token);
+        response.addHeader("Authorization",token);
     }
 }
